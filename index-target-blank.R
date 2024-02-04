3a162e2356c971998776e7d1628f50999b31bc1f
bcdanl <bchoe@geneseo.edu>
2024-02-04 03:43:30 -0500

    lec1

--- index-target-blank.R
+++ index-target-blank.R
@@ -1,83 +1,39 @@
 system("quarto render")
 quarto::quarto_render("index.qmd")
+
 library(stringr)
 
 # Read the HTML file
 html_as_text <- readLines("index.html", warn = FALSE)
 system(paste("rm", shQuote("index.html")))
+# Sys.sleep(2)
 
+# Next lecture number & dates
+lec_n <- 1   ;  lec_nos <- str_c('0', as.character(1:lec_n))
+lec_dates <- c('0206')
 
-# Replace the values
-lec_no <- '01'
-lec_date <- '0206'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
-# Replace the values
-lec_no <- '02'
-lec_date <- '0213'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
-# Replace the values
-lec_no <- '03'
-lec_date <- '0220'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
-# Replace the values
-lec_no <- '04'
-lec_date <- '0227'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
-# Replace the values
-lec_no <- '05'
-lec_date <- '0305'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
-# Replace the values
-lec_no <- '06'
-lec_date <- '0312'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
-# Replace the values
-lec_no <- '07'
-lec_date <- '0319'
-
-old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
-new_content <- str_c(old_content, " ", 'target="_blank"') 
-
-html_as_text <- str_replace_all(html_as_text,
-                                old_content,
-                                new_content)
+for (i in 1:length(lec_nos)){
+  lec_no <- lec_nos[i]
+  lec_date <- lec_dates[i]
+  
+  old_content <- str_c('lec-', lec_no, '-2024-', lec_date, '.html"')
+  new_content <- str_c(old_content, " ", 'target="_blank"') 
+  
+  
+  html_as_text <- str_replace_all(html_as_text,
+                                  old_content,
+                                  new_content)
+}
 
 writeLines(html_as_text, 
            "index.html")
 
+
+
+# file.copy("tmp/index.html", "index.html")
+
+# Use system() to execute the rm command
+# system(paste("rm", shQuote("index.html")))
+# Sys.sleep(2)
+# 
+# file.rename("index2.html", "index.html")
